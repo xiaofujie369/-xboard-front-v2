153d66e8ae3ab30deedd34bc2cbcdd11e9e8b786
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-23 04:51:22 +0800

    Create env.js

---
+++ env.js
@@ -0,0 +1,11 @@
+window.routerBase = "https://ss.shy521.com/";
+
+window.settings = {
+  title: "可云",
+  description: "可云",
+  assets_path: "/assets",
+  theme: { color: "default" },
+  version: "1.0.0",
+  background_url: "",
+  logo: "",
+};
